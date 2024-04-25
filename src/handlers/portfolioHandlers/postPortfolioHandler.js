c2e26c3202918ddc1392feae04c9fe027e96d18c
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-04-24 21:37:05 -0300

    portfolio handler

--- src/handlers/portfolioHandlers/postPortfolioHandler.js
+++ src/handlers/portfolioHandlers/postPortfolioHandler.js
@@ -5,17 +5,6 @@ const postPortfolioHandler = async (req, res) => {
 
   try {
 
-    if (
-      typeof text !== 'string') {
-      return res.status(400).send({ error: 'Incorrect DataType - text' });
-    }
-    if (typeof image !== 'string') {
-      return res.status(400).send({ error: 'Incorrect DataType - image' });
-    }
-    if (typeof video !== 'string') {
-      return res.status(400).send({ error: 'Incorrect DataType - video' });
-    }
-
     const newPortfolio = await postPortfolioCtrl(text, image, video);
 
     res.status(200).send(newPortfolio);
