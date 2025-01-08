c3cdd375f56378fe1dc909c64f507ed734739fc0
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-01-08 17:58:00 -0300

    reorder houses

--- src/controllers/houseCtrls/getHousesCtrl.js
+++ src/controllers/houseCtrls/getHousesCtrl.js
@@ -4,7 +4,18 @@ const House = require('../../collections/House');
 const getHousesCtrl = async () => {
     const houses = await House.find();
 
-    return houses;
+    // Reordenar las casas: las de tipo "eco" al inicio, el resto en el orden original.
+    const sortedHouses = houses.sort((a, b) => {
+        if (a.type === 'Eco' && b.type !== 'Eco') {
+            return -1; // "eco" antes de otros
+        }
+        if (a.type !== 'Eco' && b.type === 'Eco') {
+            return 1; // otros después de "eco"
+        }
+        return 0; // mantener el orden original entre elementos no "eco"
+    });
+
+    return sortedHouses;
 };
 
 module.exports = getHousesCtrl;
