e7901f7bd49a0431fee649b1ebad734b4de6f1f5
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-10-24 11:02:41 -0300

    get only 16 portfolios

--- src/controllers/portfolioCtrls/getPortfoliosCtrl.js
+++ src/controllers/portfolioCtrls/getPortfoliosCtrl.js
@@ -4,7 +4,16 @@ const Portfolio = require('../../collections/Portfolio');
 const getPortfoliosCtrl = async () => {
     const portfolios = await Portfolio.find();
 
-    return portfolios;
+    // Mezclar el array aleatoriamente (algoritmo de Fisher-Yates)
+    for (let i = portfolios.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [portfolios[i], portfolios[j]] = [portfolios[j], portfolios[i]];
+    }
+
+    // Tomar solo los primeros 16
+    const randomPortfolios = portfolios.slice(0, 16);
+
+    return randomPortfolios;
 };
 
 module.exports = getPortfoliosCtrl;
